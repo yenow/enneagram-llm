1472a6ef4369cf585386239409fe43cff170ee93
yun sinyoung <phantom_ysy@naver.com>
2025-09-02 16:10:16 +0900

    [Feature] LLM Prompt 추가

---
+++ app/core/logging.py
@@ -0,0 +1,33 @@
+import os
+
+def langsmith(project_name=None, set_enable=True):
+
+    if set_enable:
+        langchain_key = os.environ.get("LANGCHAIN_API_KEY", "")
+        langsmith_key = os.environ.get("LANGSMITH_API_KEY", "")
+
+        # 더 긴 API 키 선택
+        if len(langchain_key.strip()) >= len(langsmith_key.strip()):
+            result = langchain_key
+        else:
+            result = langsmith_key
+
+        if result.strip() == "":
+            print(
+                "LangChain/LangSmith API Key가 설정되지 않았습니다. 참고: https://wikidocs.net/250954"
+            )
+            return
+
+        os.environ["LANGSMITH_ENDPOINT"] = (
+            "https://api.smith.langchain.com"  # LangSmith API 엔드포인트
+        )
+        os.environ["LANGSMITH_TRACING"] = "true"  # true: 활성화
+        os.environ["LANGSMITH_PROJECT"] = project_name  # 프로젝트명
+        print(f"LangSmith 추적을 시작합니다.\n[프로젝트명]\n{project_name}")
+    else:
+        os.environ["LANGSMITH_TRACING"] = "false"  # false: 비활성화
+        print("LangSmith 추적을 하지 않습니다.")
+
+
+def env_variable(key, value):
+    os.environ[key] = value
